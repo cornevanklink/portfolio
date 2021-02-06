5e0caefa349bbc8ec3f20740da2e0b4be2907bfa
cornevanklink <cvklink@gmail.com>
2021-02-06 09:14:15 +0100

    Pychallenge 2

---
+++ js-projects/pychallenge2.js
@@ -0,0 +1,14 @@
+/*  Python Challenge 2 using Javascript 
+    http://www.pythonchallenge.com/pc/def/map.html
+    Challenge: Caesar Cipher with the transformation below (2 letters in the alphabet forward)
+    K > M
+    O > Q
+    E > G
+*/
+
+
+
+
+var input = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.";
+
+
