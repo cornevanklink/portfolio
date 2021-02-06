1cc7a9ab6f45ee7b40d7d37b8404547822e9cf54
cornevanklink <cvklink@gmail.com>
2021-02-06 09:13:41 +0100

    Added Python challenges using JS

---
+++ js-projects/pychallenge1.js
@@ -0,0 +1,8 @@
+/*  Python Challenge 1 using Javascript 
+    http://www.pythonchallenge.com/pc/def/0.html
+    Challenge: 2 to the power of 38
+    Answer: 274877906944
+*/
+
+var answer = 2**38;
+console.log(answer);
