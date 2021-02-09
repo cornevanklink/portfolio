03e08577f221e104065e5129640d0e51cb441cc9
cornevanklink <cvklink@gmail.com>
2021-02-09 10:50:26 +0100

    Moved Javascript projects to different repo

--- js-projects/pychallenge1.js
+++
@@ -1,8 +0,0 @@
-/*  Python Challenge 1 using Javascript 
-    http://www.pythonchallenge.com/pc/def/0.html
-    Challenge: 2 to the power of 38
-    Answer: 274877906944
-*/
-
-var answer = 2**38;
-console.log(answer);
